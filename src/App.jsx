1eb9a135d80d78b4fac504df5d9024f32b7db934
Yahya <yahyaabbakar@gmail.com>
2023-06-08 16:12:34 +0200

    added classes and text to description

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ export default function App() {
   return (
     <div className='container'>
       <h1>Quizzical</h1>
-      <p>Some Description</p>
-      <button>Start Quiz</button>
+      <p className='quiz-description'>Test your knowledge with our interactive quiz! Click the Start quiz button below to begin.</p>
+      <button className='quiz-button'>Start Quiz</button>
     </div>
   )
 }
