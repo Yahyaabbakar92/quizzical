bd974680cfbbe0dc7b9f5b1b9737b075d524e174
Yahya <yahyaabbakar@gmail.com>
2023-06-09 10:20:41 +0200

    mapped the data from quiz file to render in component

--- src/components/QuizQsnAns.jsx
+++ src/components/QuizQsnAns.jsx
@@ -1,7 +1,19 @@
-export default function QuizQsnAns () {
+import quiz from '../mockQsns'
+
+export default function QuizQsnAns () {   
+    const questions = quiz.results.map(({ question, correct_answer, incorrect_answers }, i) => (
+        <div 
+            key={ i }
+            className='quiz-questions-answers'
+        >
+            <p className='quiz-question'>{ question }</p>
+            <p className='quiz-answers'>{ correct_answer } { incorrect_answers.map(answers => answers) }</p>
+        </div>
+    ))
+    
     return (
-        <div>
-            <h1>Questions</h1>
+        <div className='quiz'>
+            { questions }
         </div>
     )
 }
