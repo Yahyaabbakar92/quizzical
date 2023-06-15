37fa451326b2b4da85c0d1798ec3c5b1166eedc3
Yahya <yahyaabbakar@gmail.com>
2023-06-15 08:49:39 +0200

    added the functionality to conditionally render the classes for the choices to be styled

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,43 +1,50 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
-import { decode } from 'html-entities'
+import { decode } from 'html-entities';
 
 export default function Score({ questions, onRestartQuiz }) {
-    const [showResults, setShowResults] = useState(false)
+  function renderResult(question) {
+    const isCorrect = question.selected === question.correct_answer;
 
-    function checkAnswers() {
-        setShowResults(true)
-    }
-
-    function renderResult(question) {
-        const isCorrect = question.selected === question.correct_answer
-
-        return (
-            <div key={question.id} className={`result ${isCorrect ? 'correct' : 'incorrect'}`}>
-                <h3>{decode(question.question)}</h3>
-                <ul>
-                    {question.answers.map((choice) => (
-                        <li key={choice}>
-                            <label htmlFor={choice} className='radio-label'>
-                                <input type="radio" name={question.id} id={choice} value={choice} disabled />
-                                <span>{decode(choice)}</span>
-                            </label>
-                        </li>
-                    ))}
-                </ul>
-                {isCorrect ? <p className='correct-answer'>Correct: {decode(question.correct_answer)}</p> : null }
-            </div>
-        )
-    }
     return (
-        <div className='score'>
-            <h1>Quiz Score</h1>
-            <div className='results'>
-                {showResults ? questions.map(renderResult) : null}
-            </div>
-            <button onClick={checkAnswers}>Check Answers</button>
-            <p>Number of Correct Answers: {questions.filter(question => question.selected === question.correct_answer).length}</p>
-            <button onClick={onRestartQuiz}>Restart Quiz</button>
-        </div>
-    )
+      <div key={question.id} className={`result ${isCorrect ? 'correct' : 'incorrect'}`}>
+        <h3>{decode(question.question)}</h3>
+        <ul>
+          {question.answers.map((choice) => (
+            <li key={choice}>
+              <label 
+                htmlFor={choice} 
+                className={`radio-label ${question.selected === choice ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
+              >
+                <input 
+                    type="radio" 
+                    name={question.id} 
+                    id={choice} 
+                    value={choice} 
+                    disabled 
+                />
+                <span className={isCorrect ? 'correct-answer' : ''}>{decode(choice)}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+        {!isCorrect && (
+        <p className='correct-answer'>
+          Correct: {decode(question.correct_answer)}
+        </p>
+      )}
+      </div>
+    );
+  }
+
+  const score = questions.filter((question) => question.selected === question.correct_answer).length;
+
+  return (
+    <div className='score'>
+      <div className='results'>{questions.map(renderResult)}</div>
+      <div className="score-footer">
+        <p className='score-text'>You scored {score}/{questions.length} correct answers</p>
+        <button className='restart-button' onClick={onRestartQuiz}>Restart Quiz</button>
+      </div>
+    </div>
+  );
 }
